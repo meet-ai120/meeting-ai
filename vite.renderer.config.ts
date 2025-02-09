7417bbc2e45b25129c3b93dec46820b1e0e6e3e2
Ali Toshmatov <alitoshmatov2001@gmail.com>
2025-02-09 16:25:44 +0500

    Installed vad, checking now

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -3,12 +3,20 @@ import react from "@vitejs/plugin-react";
 import type { ConfigEnv, UserConfig } from "vite";
 import { defineConfig } from "vite";
 import { pluginExposeRenderer } from "./vite.base.config";
+import type { ForgeConfig } from "@electron-forge/shared-types";
+
+interface ForgeEnv extends ConfigEnv {
+  forgeConfigSelf: NonNullable<ForgeConfig["plugins"]>[number];
+  root: string;
+}
 
 // https://vitejs.dev/config
-export default defineConfig((env) => {
-  const forgeEnv = env as ConfigEnv<"renderer">;
+export default defineConfig(async (env) => {
+  const forgeEnv = env as ForgeEnv;
   const { root, mode, forgeConfigSelf } = forgeEnv;
   const name = forgeConfigSelf.name ?? "";
+
+  const { viteStaticCopy } = await import("vite-plugin-static-copy");
 
   return {
     root,
@@ -17,7 +25,30 @@ export default defineConfig((env) => {
     build: {
       outDir: `.vite/renderer/${name}`,
     },
-    plugins: [pluginExposeRenderer(name), react()],
+    plugins: [
+      pluginExposeRenderer(name),
+      react(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: "node_modules/@ricky0123/vad-web/dist/vad.worklet.bundle.min.js",
+            dest: "./",
+          },
+          {
+            src: "node_modules/@ricky0123/vad-web/dist/silero_vad_v5.onnx",
+            dest: "./",
+          },
+          {
+            src: "node_modules/@ricky0123/vad-web/dist/silero_vad_legacy.onnx",
+            dest: "./",
+          },
+          {
+            src: "node_modules/onnxruntime-web/dist/*.wasm",
+            dest: "./",
+          },
+        ],
+      }),
+    ],
     resolve: {
       preserveSymlinks: true,
       alias: {
